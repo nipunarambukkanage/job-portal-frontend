086da934ec086060f58fab944e9499bef4cf27b3
Nipuna Rambukkanage <nipunaram@gmail.com>
2024-05-19 13:45:24 +0530

    Added grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Grid, Container } from '@material-ui/core';
+
 import theme from './theme';
 import Navbar from './components/common/Navbar';
 import Sidebar from './components/common/Sidebar';
@@ -11,36 +14,33 @@ import Categories from './pages/Categories';
 import Users from './pages/Users';
 import Login from './pages/Login';
 
-function withLayout(Component) {
-  return function WithLayout(props) {
-    return (
-      <>
-        <Navbar />
-        <Sidebar />
-        <Component {...props} />
-      </>
-    );
-  };
-}
-
-const HomeWithLayout = withLayout(Home);
-const JobsWithLayout = withLayout(Jobs);
-const JobDetailsPageWithLayout = withLayout(JobDetailsPage);
-const CategoriesWithLayout = withLayout(Categories);
-const UsersWithLayout = withLayout(Users);
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Reset browser styles for consistent layout */}
       <Router>
-        <Routes>
-          <Route path="/" element={<HomeWithLayout />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/jobs" element={<JobsWithLayout />} />
-          <Route path="/jobs/:id" element={<JobDetailsPageWithLayout />} />
-          <Route path="/categories" element={<CategoriesWithLayout />} />
-          <Route path="/users" element={<UsersWithLayout />} />
-        </Routes>
+        <Container maxWidth="false"> {/* Disable container max width for full-screen layout */}
+          <Grid container spacing={2}>
+            <Grid item xs={12}> {/* Navbar spans all columns */}
+              <Navbar />
+            </Grid>
+            <Grid container spacing={2}> {/* Nested grid for sidebar and content */}
+              <Grid item xs={2}> {/* Sidebar with fixed width */}
+                <Sidebar />
+              </Grid>
+              <Grid item xs={10}> {/* Content area takes remaining space */}
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/jobs" element={<Jobs />} />
+                  <Route path="/jobs/:id" element={<JobDetailsPage />} />
+                  <Route path="/categories" element={<Categories />} />
+                  <Route path="/users" element={<Users />} />
+                </Routes>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
       </Router>
     </ThemeProvider>
   );
